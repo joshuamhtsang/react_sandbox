a625be37a2da0488154a99ba4f507fa9665ed2a1
joshuamhtsang <joshua.mh.tsang@gmail.com>
2020-05-05 23:05:11 +0100

    added hover css :D

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,24 +2,13 @@ import React from 'react';
 
 import './Item.css'
 
-let decideButtonText = (itemType) => {
-    let text = 'USE'
-    if (itemType === 'Weapon') {
-        text = 'EQUIP'
-    }
-    return text
-}
-
 let item = (props) => {
-    let useButtonText = decideButtonText(props.type);
-
     return (
         <div className='Item' onClick={props.onClick}>
             <img src={props.picture} className="ItemPicture"/>
             <div> Name: {props.name} </div>
             <div> Type: {props.type} </div>
             {props.slot ? <div> Slot: {props.slot}  </div> : <div></div>}
-            <button onClick={props.onClick}> {useButtonText} </button>
         </div>
     )
 }
